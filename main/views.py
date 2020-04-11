cdcd4df4d01d479bce586b1bc7a948b68081cb93
ubuntu <drhtka@gmail.com>
2020-04-11 11:55:36 +0000

    step_one_dz_end

--- main/views.py
+++ main/views.py
@@ -5,22 +5,19 @@ from django.views.generic.base import View
 from main.models import Users
 
 class MainPageView(View):
+    # page registration
     def get(self, request):
         return render(request, 'main/main.html')
 
 class UsersSiteView(View):
-
+    # registration users with site
     def post(self, request):
-        print(31)
-        #print(request.GET.get('role'))
         role_id = request.POST.get('role')
         site_users = request.POST.get('username')
         site_email = request.POST.get('user_email')
         site_pass = request.POST.get('user_pass')
-        print(site_users, site_email, site_email)
         seve_site = Users(username=site_users, user_email=site_email, user_pass=site_pass, role=role_id)
         seve_site.save()
-        print(seve_site)
         #return redirect('/main')
         return render(request, 'main/user.html')
 
